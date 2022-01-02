2561116879463326991281e7fde35eb01fd1740f
theovier <theo@harkenbusch.com>
2022-01-02 15:26:15 +0100

    enable aliasing for imports

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + Tailwind 3" />
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+const path = require('path');
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {find: "@", replacement: path.resolve(__dirname, 'src')}
+    ]
+  },
   plugins: [vue()]
 })
