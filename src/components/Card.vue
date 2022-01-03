7dce68b66fcd3d6325af907ee81fd13b3e3ae272
theovier <theo@harkenbusch.com>
2022-01-03 15:55:46 +0100

    add card test

---
+++ src/components/Card.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="rounded-md"/>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,14 +1,17 @@
 <template>
-    <Hello-World msg="Hello Vue 3 + TypeScript + Vite + Tailwind 3"/>
+    <container>
+        <div class="grid grid-rows-5 grid-cols-4 gap-4">
+            <card class="bg-lime-600"/>
+            <card class="bg-blue-900" v-for="i in 5"/>
+            <card class="col-span-2 row-span-3 bg-red-900"/>
+            <card class="bg-green-900" v-for="i in 3"/>
+            <card class="row-span-2 bg-amber-900"/>
+            <card class="h-40 bg-fuchsia-900" v-for="i in 3"/>
+        </div>
+    </container>
 </template>
 
-<script>
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-    name: "Playground",
-    components: {
-        HelloWorld
-    }
-}
+<script setup>
+import Container from '@/views/Container.vue';
+import Card from '@/components/Card.vue';
 </script>
