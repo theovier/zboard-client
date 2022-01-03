5e2cc79531d43f361fc7d0240c6391a1c1820d4e
theovier <theo@harkenbusch.com>
2022-01-03 10:44:50 +0100

    add node types npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
-// @ts-ignore
-const path = require('path');
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   resolve: {
     alias: [
-      {find: "@", replacement: path.resolve(__dirname, 'src')}
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, 'src')
+      }
     ]
   },
   plugins: [vue()]
