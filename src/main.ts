75cc9e547f05b52ecafad838a1f100b874aeaec2
theovier <theo@harkenbusch.com>
2022-05-03 06:53:59 +0200

    add axios dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from "./router";
 import './assets/styles/index.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 createApp(App)
     .use(router)
+    .use(VueAxios, axios)
     .mount('#app');
