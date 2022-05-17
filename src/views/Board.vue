f7b580c75d3dfe4dd4a5f7e58861999fc3bef6db
theovier <theo@harkenbusch.com>
2022-05-17 20:06:53 +0200

    add background pattern for board

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,5 +1,6 @@
 <template>
-	<container>
+	<div class="relative p-10">
+		<div class="fixed top-0 left-0 -z-10 h-full w-full bg-hero-bubbles" />
 		<div
 			class="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
 		>
@@ -11,10 +12,18 @@
 			<card class="row-span-2" />
 			<card class="h-64" v-for="i in 3" />
 		</div>
-	</container>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import Container from "@/views/Container.vue";
 import Card from "@/components/posts/Card.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+	changeBackgroundColor();
+});
+
+function changeBackgroundColor() {
+	document.body.classList.replace("bg-white", "bg-cyan-900");
+}
 </script>
