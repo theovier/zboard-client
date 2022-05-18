6089f2dfa74b00dcb362ee1130fde30f792828d5
theovier <theo@harkenbusch.com>
2022-05-18 15:57:52 +0200

    extract system author

--- src/components/posts/PostCreateCard.vue
+++ src/components/posts/PostCreateCard.vue
@@ -3,27 +3,14 @@
 </template>
 
 <script setup lang="ts">
-import { Post, Author, Company } from "../../types";
+import { Post } from "../../types";
 import Card from "@/components/posts/Card.vue";
-
-//todo extract company, author and post
-const company: Company = {
-	id: 0,
-	name: "System",
-};
-
-const author: Author = {
-	id: 0,
-	name: "Post Bot",
-	avatar_url:
-		"https://images.unsplash.com/photo-1546776310-eef45dd6d63c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1110",
-	company: company,
-};
+import useSystemAuthor from "../../use/systemAuthor";
 
 const post: Post = {
 	id: 0,
 	title: "Scan Me to Create a New Post",
 	content: "you can also click on me in your browser.",
-	author: author,
+	author: useSystemAuthor(),
 };
 </script>
