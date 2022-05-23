4bbed2d97ecd4f59b9b93085a674c8c5f93a50f7
theovier <theo@harkenbusch.com>
2022-05-23 13:20:38 +0200

    add headlessui to style navbar

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,25 +1,77 @@
 <template>
 	<header
 		v-if="!$route.meta.hideNavbar"
-		class="hidden h-12 w-full items-center justify-end bg-slate-500 px-12 font-medium text-slate-200 sm:flex"
+		class="hidden h-12 w-full items-center justify-end bg-slate-500 px-12 text-slate-200 sm:flex"
 	>
-		<div v-if="isAuthenticated" class="flex space-x-2">
-			<div class="flex items-center space-x-6">
-				<div class="flex items-center space-x-2">
+		<div v-if="isAuthenticated" class="flex items-center">
+			<Menu as="div" class="relative">
+				<MenuButton
+					as="div"
+					class="flex cursor-pointer items-center space-x-2"
+				>
 					<img
 						id="profile-picture"
 						class="h-8 w-8 select-none rounded-full object-cover"
-						loading="lazy"
 						src="https://images.unsplash.com/photo-1517849845537-4d257902454a?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixlib=rb-1.2.1&amp;q=80&amp;raw_url=true&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=735"
 						alt="Profile Picture"
 					/>
-					<div>My Username</div>
-				</div>
-
-				<div class="cursor-pointer" @click="logout">Log out</div>
-			</div>
+					<font-awesome-icon
+						icon="caret-down"
+						class="fas cursor-pointer"
+					/>
+				</MenuButton>
+				<transition
+					enter-active-class="transition duration-100 ease-out"
+					enter-from-class="transform scale-95 opacity-0"
+					enter-to-class="transform scale-100 opacity-100"
+					leave-active-class="transition duration-75 ease-in"
+					leave-from-class="transform scale-100 opacity-100"
+					leave-to-class="transform scale-95 opacity-0"
+				>
+					<MenuItems
+						class="absolute right-0 z-20 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white text-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+					>
+						<div class="p-2">
+							<MenuItem>
+								<div
+									class="cursor-default p-2 font-normal text-gray-900"
+								>
+									Signed in as
+									<span class="font-medium">user</span>
+								</div>
+							</MenuItem>
+						</div>
+						<MenuItem @click="logout">
+							<div
+								class="group flex w-full items-center px-4 py-2"
+							>
+								<button
+									disabled
+									class="group flex w-full items-center text-gray-900 opacity-50"
+								>
+									Settings
+								</button>
+							</div>
+						</MenuItem>
+						<MenuItem v-slot="{ active }" @click="logout">
+							<div
+								class="group flex w-full items-center px-4 py-2"
+								:class="[
+									active
+										? 'bg-sky-500 text-white'
+										: 'text-gray-900',
+								]"
+							>
+								<button class="group flex w-full items-center">
+									Sign out
+								</button>
+							</div>
+						</MenuItem>
+					</MenuItems>
+				</transition>
+			</Menu>
 		</div>
-		<div v-else>
+		<div v-else class="flex space-x-2 font-medium">
 			<router-link class="px-2 py-1" :to="{ name: 'login' }">
 				Log In
 			</router-link>
@@ -32,8 +84,11 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 
 const isAuthenticated = ref(true);
 
-function logout() {}
+function logout() {
+	console.log("logout");
+}
 </script>
