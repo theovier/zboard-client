0300eafa9d715bd9a6c4a5ef70519788bd6d1a2d
theovier <theo@harkenbusch.com>
2022-06-12 18:03:36 +0200

    fix bug where non-set user profile image resulted in empty image

--- src/components/posts/PostHeader.vue
+++ src/components/posts/PostHeader.vue
@@ -1,10 +1,18 @@
 <template>
 	<div class="flex items-center space-x-4">
 		<img
+			v-if="img"
 			id="profile-picture"
 			class="h-14 w-14 select-none rounded-full object-cover"
 			loading="lazy"
 			:src="img"
+			alt="Profile Picture"
+		/>
+		<img
+			v-else
+			id="profile-picture"
+			class="h-8 w-8 select-none rounded-full object-cover"
+			src="https://images.unsplash.com/photo-1517849845537-4d257902454a?crop=entropy&amp;cs=tinysrgb&amp;fm=jpg&amp;ixlib=rb-1.2.1&amp;q=80&amp;raw_url=true&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=735"
 			alt="Profile Picture"
 		/>
 		<div class="flex-auto">
@@ -21,7 +29,7 @@
 <script lang="ts" setup>
 defineProps({
 	name: { type: String, required: true },
-	img: { type: String, default: "" },
+	img: { type: String, default: null },
 	company: { type: String, default: "Universität Paderborn" },
 });
 </script>
